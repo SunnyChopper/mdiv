88a9213bb389365656d7a6f9abc2c5dd6aa87da4
Sunny Singh <ishy.singh@gmail.com>
2018-12-27 23:27:38 -0600

    Default banner text

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -2,7 +2,11 @@
 <div class="banner-area js__parallax-window" style="background: url({{ URL::asset('img/1920x1080/02.jpg') }}) 50% 0 no-repeat fixed;">
     <div class="g-container--md g-text-center--xs g-padding-y-150--xs g-padding-y-0--md">
         <div class="g-margin-b-60--xs">
-            <h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white g-letter-spacing--1 banner-title">{{ $page_header }}</h1>   
+        	@if(isset($page_header))
+            	<h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white g-letter-spacing--1 banner-title">{{ $page_header }}</h1>
+            @else
+            	<h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white g-letter-spacing--1 banner-title">New Age Startup</h1>
+            @endif
         </div>
     </div>
 </div>
