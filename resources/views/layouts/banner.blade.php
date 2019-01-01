be4479ef9e14b955d3b26b25818c47dfeb39f39e
Sunny Singh <ishy.singh@gmail.com>
2019-01-01 12:25:28 -0600

    Added styling to blog posts

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,6 +1,6 @@
 <!--========== PROMO BLOCK ==========-->
 <div class="banner-area js__parallax-window" style="background: url({{ URL::asset('img/1920x1080/02.jpg') }}) 50% 0 no-repeat fixed;">
-    <div class="g-container--md g-text-center--xs g-padding-y-150--xs g-padding-y-0--md">
+    <div class="g-container--md g-text-center--xs pt-128-mobile g-padding-y-0--md">
         <div class="g-margin-b-60--xs">
         	@if(isset($page_header))
             	<h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white g-letter-spacing--1 banner-title">{{ $page_header }}</h1>
